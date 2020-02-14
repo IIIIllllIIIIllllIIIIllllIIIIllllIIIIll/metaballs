51dc749c46d06dad8a9de16b523be30bd0bfcaf1
Xuanji Li <xuanji@gmail.com>
2020-02-14 18:06:40 -0500

    represent circles as closure

--- metaball.js
+++ metaball.js
@@ -1,12 +1,19 @@
-var metaball = function(x, y, circles) {
-  var sum = 0;
-  for (var i = 0; i < circles.length; i++) {
-    var c = circles[i];
-    var dx = x - c.x;
-    var dy = y - c.y;
+const make_circle = function(c_x, c_y, r2) {
+  return function(x, y) {
+    var dx = x - c_x;
+    var dy = y - c_y;
 
     var d2 = dx * dx + dy * dy;
-    sum += c.r2 / d2;
+    return r2 / d2;
+  }
+}
+
+const metaball = function(x, y, circles) {
+  var sum = 0;
+  for (var i = 0; i < circles.length; i++) {
+    const c = circles[i];
+    const circle_field = make_circle(c.x, c.y, c.r2);
+    sum += circle_field(x, y);
   }
   return sum;
 };
